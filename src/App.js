423cb146b29f8426daf91e45462cab11eb90f9f8
Eraldo Sinicio <eraldosinicio@gmail.com>
2022-12-25 17:16:29 -0300

    refactor: routes

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Main } from '~/pages/Main';
-
-function App() {
-  return <Main />;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyle } from '~/components/Theme/GlobalStyle';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { CardPix } from './pages/LevelOne/CardPix';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/card-pix',
-    element: <CardPix />,
-  },
-]);
+import { App } from './pages';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
