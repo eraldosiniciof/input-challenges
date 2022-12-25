02a8234097b19cd9078f63ecfcbed7e14e2b2337
Eraldo Sinicio <eraldosinicio@gmail.com>
2022-12-25 17:41:14 -0300

    fix: responsive layout

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,12 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 import { LevelOne } from '~/pages/LevelOne';
 
+const Container = styled('div')`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+`;
+
+const Box = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 20px;
+
+  border-bottom: 1px solid #222222;
+
+  @media screen and (max-width: 450px) {
+    flex-direction: column;
+  }
+`;
+
 const Header = styled('h1')`
-  padding: 8px;
   text-align: center;
 `;
 
 const Link = styled('a')`
+  margin-left: 8px;
+  font-size: 32px;
+  font-weight: bold;
   text-decoration: none;
   color: #00bdd8;
 
@@ -18,15 +39,15 @@ const Link = styled('a')`
 
 export function Main() {
   return (
-    <>
-      <Header>
-        Desafios do{' '}
+    <Container>
+      <Box>
+        <Header>Desafios do</Header>
         <Link href="https://codar.me/input" target="_blank" rel="noreferrer">
           Curso Input | Codar.me
         </Link>
-      </Header>
+      </Box>
 
       <LevelOne />
-    </>
+    </Container>
   );
 }
