0549ca6d39d4cc8ce959b8ca9107267851a31192
Eraldo Sinicio <eraldosinicio@gmail.com>
2022-12-26 18:24:43 -0300

    refactor: comment box component

--- src/pages/LevelOne/Comment/index.js
+++ src/pages/LevelOne/Comment/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-import t from 'prop-types';
 import styled from 'styled-components';
+import { CommentBox } from '~/components/CommentBox';
 
-import { Icon } from '~/components/Icon';
 import { Plus } from '~/components/Plus';
 
 const mockData = [
@@ -38,141 +37,6 @@ const CenterBox = styled('div')`
   gap: 16px;
 `;
 
-const Card = styled('div')`
-  display: flex;
-  align-items: center;
-
-  justify-content: flex-start;
-
-  padding: 12px 56px;
-
-  width: 720px;
-  height: 128px;
-  border-radius: 32px;
-
-  background-color: #ffffff;
-
-  @media screen and (max-width: 400px) {
-    padding: 10px 20px;
-    width: 330px;
-    height: 10vh;
-  }
-`;
-
-const BoxAvatar = styled('div')`
-  background-color: #f5cb5c;
-  border-radius: 42px;
-  width: 85px;
-  height: 85px;
-  overflow: hidden;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  @media screen and (max-width: 400px) {
-    border-radius: 30px;
-    width: 70px;
-    height: 70px;
-  }
-`;
-
-const Box = styled('div')`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  gap: 8px;
-
-  margin-left: 40px;
-
-  @media screen and (max-width: 400px) {
-    margin-left: 8px;
-  }
-`;
-
-const Title = styled('div')`
-  font-size: 24px;
-  font-weight: bold;
-  color: #18161f;
-
-  @media screen and (max-width: 400px) {
-    font-size: 20px;
-  }
-`;
-
-const Hours = styled('div')`
-  color: rgba(24, 22, 31, 0.6);
-  font-size: 24px;
-  font-weight: 700;
-
-  @media screen and (max-width: 400px) {
-    font-size: 12px;
-  }
-`;
-
-const BoxHeader = styled('div')`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-
-  @media screen and (max-width: 400px) {
-    gap: 4px;
-  }
-`;
-
-const BoxComment = styled('div')`
-  color: #18161f;
-  font-size: 24px;
-
-  @media screen and (max-width: 400px) {
-    font-size: 16px;
-  }
-`;
-
-function getDiffHours(initialDate) {
-  const created = new Date(initialDate).getTime();
-  const now = new Date().getTime();
-
-  const diff = new Date(now - created);
-  const diffHours = Math.round(diff.getTime() / 3600000).toString();
-
-  const totalDays = (diffHours / 24).toFixed(0);
-
-  const result =
-    diffHours > 24
-      ? `${totalDays} ${totalDays > 1 ? 'dias' : 'dia'}`
-      : `${diffHours} ${diffHours > 1 ? 'horas' : 'hora'}`;
-
-  return `● ${result}`;
-}
-
-function Item({ data }) {
-  return (
-    <>
-      {data.length &&
-        data.map((dataComment) => (
-          <Card key={dataComment.name}>
-            <BoxAvatar>
-              <Icon name={dataComment.avatar || 'avatar'} />
-            </BoxAvatar>
-            <Box>
-              <BoxHeader>
-                <Title>{dataComment.name}</Title>
-                <Hours>{getDiffHours(dataComment.created_at)} atrás</Hours>
-              </BoxHeader>
-              <BoxComment>{dataComment.comment}</BoxComment>
-            </Box>
-          </Card>
-        ))}
-    </>
-  );
-}
-
-Item.propTypes = {
-  data: t.arrayOf(t.any),
-};
-
 const plusDescription = [
   'Fiz um mockup de dados com estrutura parecidas com as que são recebidas pelo backend. Os dois comentários abaixo estão sendo renderizados por um map.',
   'O primeiro comentário tem avatar=null. Sendo assim renderiza um avatar padrão',
@@ -184,7 +48,7 @@ export function Comment() {
     <Container>
       <Plus description={plusDescription} />
       <CenterBox>
-        <Item data={mockData} />
+        <CommentBox data={mockData} />
       </CenterBox>
     </Container>
   );
