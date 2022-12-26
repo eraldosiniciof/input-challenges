10ee1df0dd50f055f81a2a174ca4856e3bd37dd7
Eraldo Sinicio <eraldosinicio@gmail.com>
2022-12-26 16:49:05 -0300

    feat: level 2 - menu sidebar

--- src/pages/LevelTwo/index.js
+++ src/pages/LevelTwo/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Option } from '~/components/Option';
 
 const Container = styled('div')`
   padding: 20px;
@@ -11,6 +12,12 @@ export function LevelTwo() {
   return (
     <Container>
       <Title>Desafios de Nível 2 🚧</Title>
+
+      <Option
+        name="Menu"
+        to="/menu"
+        description="Desenvolver um Menu de um site"
+      />
     </Container>
   );
 }
